797420b52517ffbef162074618a2409a30c29438
rayjonesjay <rayjaymuiruri@gmail.com>
2024-05-08 21:32:32 +0300

    ascii art

---
+++ args/parser_test.go
@@ -0,0 +1,63 @@
+package args
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Given a testing context, runs tests on the [Parse] function with the given input ensuring the tests only passes
+// when the [Parse] function produces the expected output
+func helperParse(t *testing.T, input []string, expect []DrawInfo) {
+	output := Parse(input)
+	if !reflect.DeepEqual(output, expect) {
+		t.Errorf("Expected %v, got %v\n", expect, output)
+	}
+}
+
+func TestParse(t *testing.T) {
+	// Default case, parse text, to be printed, as the only command line argument
+	// go run . "Hello"
+	input := []string{"Hello"}
+	// We expect the text to be mapped to the default standard banner style
+	expect := []DrawInfo{{"Hello", Standard}}
+	output := Parse(input)
+	// Compare the expected output array, with the actual output array
+	if !reflect.DeepEqual(output, expect) {
+		t.Errorf("Expected %v, got %v\n", expect, output)
+	}
+
+	// Other cases using the test helper function
+
+	// 0. go run .
+	// No text specified, hence nothing to draw
+	helperParse(t, []string{}, nil)
+
+	// 1. go run . "\n"
+	helperParse(t, []string{"\n"}, []DrawInfo{{"\n", Standard}})
+
+	// 2. go run . "Hello" standard "World" shadow
+	helperParse(t,
+		[]string{"Hello", "standard", "World", "shadow"},
+		[]DrawInfo{{"Hello", Standard}, {"World", Shadow}},
+	)
+
+	// 3. go run . "Hello" standard "There" shadow "World" thinkertoy
+	helperParse(t,
+		[]string{"Hello", "standard", "There", "shadow", "World", "thinkertoy"},
+		[]DrawInfo{{"Hello", Standard}, {"There", Shadow}, {"World", Thinkertoy}},
+	)
+
+	// 4. go run . "Hello" standard "There"
+	// If the last text's style is omitted, we assume it is the default standard style
+	helperParse(t,
+		[]string{"Hello", "standard", "There"},
+		[]DrawInfo{{"Hello", Standard}, {"There", Standard}},
+	)
+
+	// 5. go run . "Hello" thinkertoy "There" shadow "World"
+	// If the last text's style is omitted, we assume it is the default standard style
+	helperParse(t,
+		[]string{"Hello", "thinkertoy", "There", "shadow", "World"},
+		[]DrawInfo{{"Hello", Thinkertoy}, {"There", Shadow}, {"World", Standard}},
+	)
+}
---
+++ special/chars.go
@@ -0,0 +1,134 @@
+package special
+
+import "strings"
+
+// handling \b
+func SlashB(s string) string {
+	result := ""
+
+	// change the string to an array of runes
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		// check if the current rune is not at the last index and it's \ and the next is b
+		if i+1 < len(runes) && runes[i] == '\\' && runes[i+1] == 'b' && i+1 != len(runes)-1 {
+			// remove the last element concatinated in the string "result"
+			if len(result) > 0 {
+				result = result[:len(result)-1]
+			}
+			i++ // skip the '\' and 'b' character
+		} else {
+			result += string(runes[i])
+		}
+	}
+
+	// when \b is at the end of the string, ignore
+	for i, v := range result {
+		if v == '\\' && result[i+1] == 'b' {
+			result = result[:len(result)-2]
+		}
+	}
+	return result
+}
+
+// handling \0
+func SlashZero(s string) string {
+	result := ""
+
+	// change the string to an array of runes
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		// check if the current rune is not at the last index and it's '\' and the next is '0'
+		if i+1 < len(runes) && runes[i] == '\\' && runes[i+1] == '0' {
+			i++ // skip the '\' and '0' character
+		} else {
+			result += string(runes[i])
+		}
+	}
+	return result
+}
+
+// handling \r
+func SlashR(s string) string {
+	result := ""
+	arr := strings.Split(s, "\\r")
+
+	for i := 0; i < len(arr); i++ {
+		// if there is no \r character in the string then the arr will only have one string
+		// in that case just return the string
+		if len(arr) == 1 {
+			return arr[0]			
+		}
+
+		// check if the string after \r is longer than the string before
+		// if so, update the string before to be the string after \r
+		if i+1 < len(arr) && len(arr[i+1]) > len(arr[i]) {
+			if len(result) > 0 {
+				result = ""
+			}
+			result += arr[i+1]
+		}
+
+		if i+1 < len(arr) {
+			// if result is empty, update it to the string at the current index
+			if len(result) < 1 {
+				result = arr[i]
+			}
+
+			// prepend the string after \r as you overwrite to the string before
+			result = arr[i+1] + result[len(arr[i+1]):]
+		}
+	}
+	return result
+}
+
+// handling \f and \v
+func SlashFSlashV(s string) string {
+	// spaces will hold spaces for indentation
+	spaces := ""
+
+	// if the last character of the string is a form feed ('\f') or vertical tab ('\v'), add a space to ensure the last line is properly handled
+	if s[len(s)-1] == '\f' || s[len(s)-1] == '\v' {
+		s += " "
+	}
+
+	// split the string into fields using the Split function, which splits using 2 delimiters, \f and \v
+	arr := strings.FieldsFunc(s, Split)
+
+	// iterate over the splitted array of strings
+	for i := 0; i < len(arr); i++ {
+		// for the second line calculate the number of spaces needed for indentation
+		if i == 0 {
+			for j := 1; j <= len(arr[i]); j++ {
+				spaces += " "
+			}	
+		} else {
+			// for the rest of the lines, the number of spaces will be less one to avoid counting the \n character
+			for j := 1; j <= len(arr[i])-1; j++ {
+				spaces += " "
+			}
+		}
+
+		// prevent panic error of array out of range by ensuring i is not the last index in the array
+		if i+1 < len(arr) {
+			// add the necessary indentations to the subsequent items
+			arr[i+1] = "\n" + spaces + arr[i+1]
+		}
+
+		// reset the spaces for the next iteration
+		spaces = ""
+	}
+
+	// join the array of strings into a single string
+	result := strings.Join(arr, "")
+
+	// if the original string ended with a space, remove it from the result
+	if s[len(s)-1] == ' ' {
+		return result[0:len(result)-1]
+	}
+	return result
+}
+
+// Split is a function used by strings.FieldsFunc to enable splitting using 2 delimiters
+func Split(r rune) bool {
+	return r == '\f' || r == '\v'
+}
