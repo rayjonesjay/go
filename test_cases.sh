767d97d242346e3f1b625b0e50bea1befafb9893
najwang <anyonanicholas86@gmail.com>
2024-05-15 17:11:03 +0300

    test: add more program test cases

--- test_cases.sh
+++ test_cases.sh
@@ -29,3 +29,12 @@ echo -e '\x61\x62\x0A\x61\x62\x0A'
 
 echo -e 't\x61\x62b\x0At\x61\x62b'
 echo -e 't\x61\x62b\x0At\x61\x62b\x0A'
+
+echo -e "Hello\f\fWorld\fThere\fab"
+echo -e "Hello\f\fWorld\fThere\f"
+
+echo -e "Ring The Bell\a"
+echo -e 'hello There 1 to 2!' # expect "hello There 1 to 2!"
+echo -e "1a\"#FdwHywR&/()=" # expect "1a"#FdwHywR&/()="
+
+echo -e '\!" #$%&'"'"'()*+,-./' # expect "\!" #$%&'()*+,-./"
