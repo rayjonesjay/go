59aa70a0e8d2dd68299e29fec8bf81f292fc0c33
najwang <anyonanicholas86@gmail.com>
2024-06-01 16:24:12 +0300

    docs: add details to document the Beep function

--- sound/play.go
+++ sound/play.go
@@ -8,11 +8,11 @@ import (
 
 // Beep plays a beep sound. To do this, the target system must have the program 'paplay' installed
 // and that the file: '/usr/share/sounds/freedesktop/stereo/bell.oga' exists and is a valid OGG file
+// Linux has the program, paplay, that plays a given audio file of the supported file formats including .oga files
+// We can, thus, play the default bell sound, preinstalled by freedesktop
+// (an open desktop standard followed by most windowing systems, including Ubuntu), by executing the command:
+// `paplay /usr/share/sounds/freedesktop/stereo/bell.oga`
 func Beep() {
-	// Linux has the program, paplay, that plays a given audio file of the supported file formats including .oga files
-	// We can, thus, play the default bell sound, preinstalled by freedesktop
-	//(an open desktop standard followed by most windowing systems, including Ubuntu), with the command:
-	//paplay /usr/share/sounds/freedesktop/stereo/bell.oga
 	paplay := "paplay"
 	bell := "/usr/share/sounds/freedesktop/stereo/bell.oga"
 	cmd := exec.Command(paplay, bell)
