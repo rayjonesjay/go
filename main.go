05d624bf269939fa1a952a40d64421f56fe85627
najwang <anyonanicholas86@gmail.com>
2024-06-01 16:48:43 +0300

    fix: allow running program with no args

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
+	mArgs := os.Args[1:]
+	if len(mArgs) == 0 {
+		return
+	}
 
-	draws, outputFile := args.Parse(os.Args[1:])
+	draws, outputFile := args.Parse(mArgs)
 	if draws == nil {
 		// nothing to draw
 		help.PrintUsage()
 	}
 	output.Draw(draws, outputFile)
-
 }
