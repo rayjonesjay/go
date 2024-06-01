4fa5dd06adccd6bd9ace9b338e0b928a543fc788
najwang <anyonanicholas86@gmail.com>
2024-06-01 16:36:41 +0300

    style: prefer doc comments to raw comments

--- graphics/graphics_test.go
+++ graphics/graphics_test.go
@@ -2,18 +2,18 @@ package graphics
 
 import "testing"
 
+// The caret is basically a graphics line, which in essence is actually 8 lines as below
+// -------------------------------------
+// aaa
+// bbb
+// ccc
+// ddd
+// eee
+// fff
+// ggg
+// hhh
+// --------------------------------------
 func TestSPrintCaret(t *testing.T) {
-	// The caret is basically a graphics line, which in essence is actually 8 lines as below
-	// -------------------------------------
-	// aaa
-	// bbb
-	// ccc
-	// ddd
-	// eee
-	// fff
-	// ggg
-	// hhh
-	//--------------------------------------
 	caret := []string{"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"}
 	expected := "aaa\nbbb\nccc\nddd\neee\nfff\nggg\nhhh"
 	output := SPrintCaret(caret)
