51c20040d2eb431734910bfa1a0ff8d1d5d74b24
najwang <anyonanicholas86@gmail.com>
2024-06-03 22:32:20 +0300

    feat: default to printing the short program usage help

--- help/usage.go
+++ help/usage.go
@@ -5,14 +5,29 @@ import (
 	"os"
 )
 
-// PrintUsage Prints the program usage to the standard output, then exits the program with a non-zero return code
+// PrintUsage prints a short program usage to the standard output, then exits the program with a non-zero return code
 func PrintUsage() {
-	usage, err := os.ReadFile("plain/usage.txt")
+	printUsage("plain/usage.txt", 1)
+}
+
+// PrintLongUsage prints the full program usage to the standard output,
+// then exits the program with a non-zero return code
+func PrintLongUsage() {
+	printUsage("plain/usage.txt", -1)
+	printUsage("plain/usage_long.txt", 1)
+}
+
+// printUsage prints the program usage as defined in the given file to the standard output,
+// then exits the program with the given return code
+func printUsage(fileName string, exitCode int) {
+	usage, err := os.ReadFile(fileName)
 	if err != nil {
 		// We couldn't read the usage text our program was shipped with!
 		_, _ = fmt.Fprintln(os.Stderr, "Improper program installation. Re-installation recommended!!")
 		os.Exit(1)
 	}
 	fmt.Print(string(usage))
-	os.Exit(0)
+	if exitCode >= 0 {
+		os.Exit(exitCode)
+	}
 }
