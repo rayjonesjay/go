b1c5caee6e9e924614756eafc42c6541276fd862
najwang <anyonanicholas86@gmail.com>
2024-06-04 01:04:00 +0300

    feat: automatic download of banner files if missing

--- graphics/draw.go
+++ graphics/draw.go
@@ -2,95 +2,73 @@ package graphics
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
-)
-
-var (
-	FAIL = func() { os.Exit(1) }
-
-	PASS = func() { os.Exit(0) }
 )
 
 const (
 	CurrentWorkingDirectory = "$PWD/"
-	PathToUrlFile           = CurrentWorkingDirectory + "plain/urls.txt"
-	PathToBannerFiles       = CurrentWorkingDirectory + "banners/"
+	bannersDir              = "banners/"
 )
 
-// checkFileExist checks if the specified banner file exists in the banner/ directory,
-// offers to download the banner file if it does not yet existCURRENT_WORKING_DIRECTORY
-func checkFileExist(fileName string) string {
-	FileInformation, err := os.Stat(fileName)
+// checkFileExist checks if the specified banner file exists in the banner/ directory and that it is not an empty file;
+// offers to download the banner file if it does not yet exist
+func checkFileExist(filePath, fileName string) {
+	fileInfo, err := os.Stat(filePath)
 
-	// Check if the file exists and has a size of 0 if true download automatically
-	if FileInformation != nil && FileInformation.Size() == 0 {
-		deleteEmptyBanner(fileName)
-		download(fileName)
-	}
-
-	// if the file does not exist download
 	if err != nil {
+		// file does not exist, download
+		download(fileName)
+	} else if fileInfo != nil && fileInfo.Size() == 0 {
+		// download the banner file if it exists and is an empty file
+		deleteEmptyBanner(filePath)
 		download(fileName)
 	}
-
-	return fileName
 }
 
-// DirectoryExist() checks if a directory exist
+// directoryExist checks if the given directory exists
 func directoryExist(dirPath string) bool {
 	_, err := os.Stat(dirPath)
 	// IsNotExist() will return true iff the directory/file at the given file path does not exist
 	return !os.IsNotExist(err)
 }
 
-// deleteEmptyBanner removes the specified empty banner file with no ascii art,
-// and it is called before calling download to avoid conflict
-func deleteEmptyBanner(filename string) {
-	filePath := CurrentWorkingDirectory + filename
+// deleteEmptyBanner removes the specified empty banner file, possibly
+// before calling download to avoid conflict
+func deleteEmptyBanner(fileName string) {
+	filePath := CurrentWorkingDirectory + fileName
 	filePath = os.ExpandEnv(filePath)
 	rm := exec.Command("rm", filePath)
 	_, err := rm.CombinedOutput()
 	if err != nil {
-		log.Fatalf("error deleting %v", err)
+		log.Fatalf("failed to delete empty banner: %q\n%v\n", fileName, err)
 	}
 }
 
-// download banner files from "https://learn.zone01kisumu.ke/git/root/public/src/branch/master/subjects/ascii-art/"
+// download banner files from https://learn.zone01kisumu.ke/git/root/public/src/branch/master/subjects/ascii-art/
 func download(fileName string) {
+	if !Contains([]string{"standard.txt", "shadow.txt", "thinkertoy.txt"}, fileName) {
+		log.Fatalf(
+			"%q is not a known banner file; download it from your source and add it to the %q directory\n",
+			fileName, bannersDir,
+		)
+	}
+
 	rawLink := "https://learn.zone01kisumu.ke/git/root/public/raw/branch/master/subjects/ascii-art/"
-	file := strings.ReplaceAll(fileName, "banners/", "")
-	fullPath := rawLink + file
+	fullPath := rawLink + fileName
 	downloadCommand := exec.Command("wget", fullPath)
-	_, err := downloadCommand.CombinedOutput()
+	output, err := downloadCommand.CombinedOutput()
 
 	if err != nil {
-		log.Fatalf("\n\tdownload attempt failed! check your internet connection and try again.\n")
+		log.Fatalf("failed to download banner: %q\n%s\n", fileName, output)
 	} else {
-
-		move := exec.Command("mv", file, "banners/")
-		output, err := move.CombinedOutput()
+		move := exec.Command("mv", fileName, "banners/")
+		_, err := move.CombinedOutput()
 		if err != nil {
-			// check error occurred when moving file to banners/
-			fmt.Fprintf(os.Stderr, "error: %T\n", err)
-			fmt.Println(output)
+			log.Fatalf("failed to move banner: %q to %q directory\n%v\n", fileName, bannersDir, err)
 		}
 	}
-}
-
-// fixFileExtension removes additional extensions from a file and returns only the first extension
-func fixFileExtension(fileName string) string {
-	// the purpose of this part is to remove the repetitive extension if any for example file.txt.txt -> file.txt
-	// file extension - a group of words occurring after a (.) period character indicating the format of the file
-	position := strings.Index(fileName, ".")
-	if position != -1 {
-		fileName = fileName[:position] + ".txt"
-	}
-	return fileName
 }
 
 // makeDirectory creates the banner directory if it does not exist
@@ -99,49 +77,27 @@ func makeDirectory(dir string) {
 	dir = os.ExpandEnv(path)
 	err := os.Mkdir(dir, 0o775)
 	if err != nil {
-		log.Fatalf("\npath error %s\n\t%v\n", path, err)
+		log.Fatalf("failed to create banners directory at %q\n%v\n", path, err)
 	}
 }
 
 // ReadBanner This function takes the name of a file in string format,
 // reads the ascii art inside the file, and maps each ascii art to its rune value
-func ReadBanner(filePath string) map[rune][]string {
-	fileName := strings.ReplaceAll(filePath, "banners/", "")
-	fileName = fixFileExtension(fileName)
-	if !(fileName == "standard.txt" || fileName == "shadow.txt" || fileName == "thinkertoy.txt") {
-		fmt.Printf(
-			"%s not a valid banner file. Download it from your source and add it to banner/ directory.\n",
-			fileName,
-		)
-		os.Exit(1)
-	}
-	// split the directory and file apart
-	dir, _ := filepath.Split(filePath)
+func ReadBanner(fileName string) map[rune][]string {
+	filePath := bannersDir + fileName
 
-	// if the directory does not exist
-	if !directoryExist(dir) {
-		// create directory regardless
-		makeDirectory(dir)
-	} else {
-		// check for file existence
-		filePath = checkFileExist(filePath)
+	// create banners directory if it does not exist
+	if !directoryExist(bannersDir) {
+		makeDirectory(bannersDir)
 	}
+	checkFileExist(filePath, fileName)
 
 	file, openingFileError := os.Open(filePath)
 	// handle error if the file does not exist error and also other possible errors
 	if openingFileError != nil {
-		if os.IsNotExist(openingFileError) {
-			download(fileName)
-			file, openingFileError = os.Open(filePath)
-			if openingFileError != nil {
-				log.Fatalf("Failed to open banner file: \"%s\"", filePath)
-			}
-		} else {
-			log.Fatalf("Failed to open banner file: \"%s\"", filePath)
-		}
+		log.Fatalf("failed to open banner file: %q\n%v", filePath, openingFileError)
 	}
-
-	defer file.Close()
+	defer Close(file)
 
 	// create a scanner object
 	scan := bufio.NewScanner(file)
@@ -150,7 +106,6 @@ func ReadBanner(filePath string) map[rune][]string {
 
 	// variable i is our loop counter, and it starts from 32 which is space character
 	for i := 32; i <= 126; i++ {
-
 		currentRune := rune(i)
 		// skip one line
 		scan.Scan()
@@ -171,6 +126,20 @@ func ReadBanner(filePath string) map[rune][]string {
 
 		asciiMap[currentRune] = currentArt
 	}
-	// Create an array of arrays to represent four spaces, each eight lines long
 	return asciiMap
+}
+
+// Close the given file
+func Close(file *os.File) {
+	_ = file.Close()
+}
+
+// Contains checks whether the string s is an element of the given array
+func Contains(arr []string, s string) bool {
+	for _, a := range arr {
+		if a == s {
+			return true
+		}
+	}
+	return false
 }
