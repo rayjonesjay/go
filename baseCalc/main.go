6a021df0e561705a41ce75044c3e35dacbc9cc4a
rayjonesjay <rayjaymuiruri@gmail.com>
2024-06-29 23:56:38 +0300

    Driver main function

--- baseCalc/main.go
+++ baseCalc/main.go
@@ -1,9 +1,17 @@
 package main 
 
 import (
-	"fmt"
+	"bufio"
+	"os"
+	"baseCalc/Read"
 )
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	writer := bufio.NewWriter(os.Stdout)
+
+	// welcome message for user
+	writer.WriteString("welcome to base converter by https://github.com/rayjonesjay/\ntype \"quit or .q\" to exit the program.\ndefault mode=10->2\n")
+	writer.WriteString("--------------------------------------------\n")
+	writer.Flush()
+	Read.ReadExpression()
 }
