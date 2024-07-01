6a9996842d457010cef0cdf8b66f60a8371435c6
rayjonesjay <rayjaymuiruri@gmail.com>
2024-07-02 01:28:52 +0300

    imported the CurrentMode struct type to main to keep track of modes

--- baseCalc/main.go
+++ baseCalc/main.go
@@ -1,16 +1,17 @@
-package main 
+package main
 
 import (
-	"bufio"
-	"os"
 	"baseCalc/Read"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
 	writer := bufio.NewWriter(os.Stdout)
-
 	// welcome message for user
-	writer.WriteString("welcome to base converter by https://github.com/rayjonesjay/\ntype \"quit or .q\" to exit the program.\ndefault mode=10->2\n")
+	someMessage := fmt.Sprintf("\ndefault mode=%d->%d\n",Read.CurrentMode.From,Read.CurrentMode.To)
+	writer.WriteString("welcome to base converter by https://github.com/rayjonesjay/\ntype \"quit or .q\" to exit the program."+someMessage)
 	writer.WriteString("--------------------------------------------\n")
 	writer.Flush()
 	Read.ReadExpression()
